6b1300088da2c8ebaf629681ca139ce8c35367fa
Adrian Booth <Adrian.booth17@gmail.com>
2016-02-14 15:48:25 +0000

    Response stored in session and now rendered on index page

--- app/views.py
+++ app/views.py
@@ -1,16 +1,24 @@
-from flask import render_template, redirect
+from flask import Flask, session, redirect, url_for, escape, request, render_template
 from app import app 
 import twitter_stream
+import os
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+	if 'response' in session:
+		response=session['response']
+    		return render_template("index.html",response=response)
+    	return render_template("index.html")
+
 
 @app.route("/apology_counter", methods = ["POST"])
 def apology_counter():
-	return redirect("/apologies")
+	session['response'] = twitter_stream.find_tweets()
+	return redirect("/")
 
-@app.route("/apologies")
-def apologies():
-	response = twitter_stream.find_tweets()
-    	return render_template("apologies.html",response=response)
+# @app.route("/apologies")
+# def apologies():
+# 	response = twitter_stream.find_tweets()
+#     	return render_template("apologies.html",response=response)
+
+app.secret_key = os.urandom(32)
